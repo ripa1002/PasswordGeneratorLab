02b53ef6b75ed84e18629867ce2faac13b53b992
Risto Sutinen <risto.sutinen@gmail.com>
2019-09-09 21:38:14 +0300

    First draft. MD5 with MessageDigest

---
+++ PasswordGeneratorLab/src/main/java/risto/tira/passwordgeneratorlab/Generator.java
@@ -0,0 +1,30 @@
+package risto.tira.passwordgeneratorlab;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.xml.bind.DatatypeConverter;
+
+public class Generator {
+    
+    public static String md5(String inputString) throws NoSuchAlgorithmException {
+        //Using MD5 algorithm.
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        
+        //Encode inputString into a sequence of bytes, store result into a new byte array.
+        //Update message digest using inputString byte array.
+        md5.update(inputString.getBytes());
+        
+        //Completes the hash computation.
+        //Store result to digest variable.
+        byte[] digest = md5.digest();
+        
+        //Convert digest byte array into String.
+        String myHash = DatatypeConverter.printHexBinary(digest).toUpperCase();
+        
+        return myHash;
+    }
+    
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        System.out.println(md5("salasana"));
+    }
+}
